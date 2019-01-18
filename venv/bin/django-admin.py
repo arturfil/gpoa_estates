dcc42135eb97ee08b08bd40e96fff3e522aea5c6
Arturo Filio Villa <arturfil@hotmail.com>
2019-01-18 10:44:04 -0500

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/arturofiliovilla/Documents/python-projects/gpoa_estate/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
