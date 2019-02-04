51519bf119272fd44122a78ecd5ad96473de1eac
Arturo Filio Villa <arturfil@hotmail.com>
2019-02-04 11:09:14 -0500

    added dictionaries and dynamic options in the index and search route

--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.choices import price_choices, bedroom_choices, state_choices
 from listings.models import Listing
 
 def index(request):
   listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
   context = {
-    'listings': listings
+    'listings': listings,
+    'state_choices': state_choices,
+    'bedroom_choices': bedroom_choices,
+    'price_choices': price_choices,
   }
 
   return render(request, 'pages/index.html', context)
