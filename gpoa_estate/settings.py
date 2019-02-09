e1077af0fd5ec52c0968da9c56470cc9629b4aa4
Arturo Filio Villa <arturfil@hotmail.com>
2019-02-09 11:40:45 -0500

    added feature for the search bar

--- gpoa_estate/settings.py
+++ gpoa_estate/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'inquilinos.apps.InquilinosConfig',
+    'accounts.apps.AccountsConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
